f205916f8637cc60fd782fb96be511c9294c6335
Christabella Irwanto <christabella.irwanto@gmail.com>
2019-04-20 01:57:45 +0300

    sparse_coding: A leedle bitta refactoring; Analysis of atoms

--- sparse_coding/sparse_coding.py
+++ sparse_coding/sparse_coding.py
@@ -6,6 +6,10 @@ from datetime import timedelta
 from gensim.models import KeyedVectors
 from sklearn.decomposition import MiniBatchDictionaryLearning
 
+TRAIN = True
+ANALYZE = True
+words = ["bank", "cut", "bass", "tie"]
+
 def sample_embeddings(model_wv, words=[], restrict_vocab=10000):
     embeddings = pd.DataFrame(model_wv.wv.syn0[:restrict_vocab])
     for i, word in enumerate(words):
@@ -19,20 +23,37 @@ model_wv = KeyedVectors.load_word2vec_format('../whatever2vec/w2n2v.embeddings',
 
 # Only sample N word embeddings
 selected_df = sample_embeddings(model_wv,
-                                words=["bank", "cut", "bass", "tie"],
+                                words=words,
                                 restrict_vocab=1000)
 print(selected_df.shape)
 
-# Train dictionary learning model
-num_cpus = multiprocessing.cpu_count()
-X = selected_df.values
-dictionary = MiniBatchDictionaryLearning(n_components=2000, fit_algorithm='lars', transform_algorithm='lars',
-                                         transform_n_nonzero_coefs=5, verbose=1, n_jobs=num_cpus, batch_size=16, n_iter=1000)
-start = time.time()
-dictionary.fit(X)
-elapsed = time.time() - start
-print("Dictionary learning took " + timedelta(seconds=elapsed))
+if TRAIN:
+    # Train dictionary learning model
+    num_cpus = multiprocessing.cpu_count()
+    X = selected_df.values
+    dictionary = MiniBatchDictionaryLearning(n_components=2000, fit_algorithm='lars', transform_algorithm='lars',
+                                             transform_n_nonzero_coefs=5, verbose=1, n_jobs=num_cpus, batch_size=16, n_iter=1000)
+    start = time.time()
+    dictionary.fit(X)
+    elapsed = time.time() - start
+    print("Dictionary learning took " + str(timedelta(seconds=elapsed)))
 
-# Save model
-with open('dictionary.model', 'wb') as f:
-    pickle.dump(model, f)
+    # Save model
+    with open('dictionary.model', 'wb') as f:
+        pickle.dump(model, f)
+
+# Analyze "atoms of discourse"
+if ANALYZE:
+    with open("dictionary.model", 'rb') as f:
+        dictionary = pickle.load(f)
+    basis_vectors = dictionary.components_
+    for word in words:
+        embedding = model_wv.get_vector(word)
+        sparse_code = dictionary.transform(embedding)
+        activated_atoms = sparse_code > 0
+        print("\tAtoms: " + str(np.arange(len(sparse_code))[activated_atoms]))
+        activated_atoms = dictionary.components_[activated_atoms]
+        # Get closest words to each atom of discourse
+        for i, atom in enumerate(activated_atoms):
+            similar_words = model_wv.similar_by_vector(atom, topn=10)
+            print(similar_words)
