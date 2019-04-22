e9f6c5a56f34e59feb567b42ee560ea0bd0c9172
Christabella Irwanto <christabella.irwanto@gmail.com>
2019-04-22 14:16:34 +0300

    sparse_coding: Plot full tables

--- sparse_coding/sparse_coding.py
+++ sparse_coding/sparse_coding.py
@@ -16,14 +16,16 @@ EMBEDDING_SIZE = 50
 VOCAB_SIZE = 10000
 # COMPONENT_SIZE = 2000
 COMPONENT_SIZE = 500
-test_words = ["bank", "bass", "tie", "chips", "mouse", "crane", "spring", "light", "star", "helsinki", "finland"]
+test_words = ["bank", "bass", "tie", "chips", "mouse", "crane", "spring",
+              "light", "star", "helsinki", "finland"]
 
 parser = argparse.ArgumentParser(description=None)
 parser.add_argument('--model', default='', type=str,
-                    choices=('word2node2vec', 'word2vec', 'lm'),
+                    choices=('word2node2vec', 'word2vec', 'lm2vec'),
                     help='Type of word embedding model to load')
 args = parser.parse_args()
 DICTIONARY_FILENAME = f"dict_{args.model}_{EMBEDDING_SIZE}_vocab={VOCAB_SIZE}_components={COMPONENT_SIZE}.model"
+
 
 def sample_embeddings(model_wv, words=[], restrict_vocab=10000):
     embeddings = pd.DataFrame(model_wv.wv.syn0[:restrict_vocab])
@@ -32,6 +34,20 @@ def sample_embeddings(model_wv, words=[], restrict_vocab=10000):
         # Just overwrite
         embeddings.iloc[i] = embedding
     return embeddings
+
+def get_sparse_code(dictionary, model_wv, word):
+    """Get the activated atoms and sparse codes for a word."""
+    embedding = model_wv.get_vector(word)  # (50, )
+    embedding = embedding.reshape(1, -1)  # (1, 50)
+    sparse_code = dictionary.transform(embedding)  # (1, 2000)
+    sparse_code = sparse_code.flatten()  # (2000, )
+    atom_indices = np.where(sparse_code > 0)
+    print("\n\tAtoms: " + str(atoms_indices))
+    # The activated atoms' embddings are the dictionary's basis vectors
+    # with non-zero coefficients
+    atom_embeddings = dictionary.components_[sparse_code > 0]
+    return atom_indices, atom_embeddings
+
 
 # Load Word2VecKeyedVectors object
 if args.model == 'word2node2vec':
@@ -82,16 +98,11 @@ if IS_ANALYSIS:
     for word in test_words:
         demarcations.append(demarcation_i)
         print(f"\n\tWord: {word}")
-        embedding = model_wv.get_vector(word)  # (50, )
-        sparse_code = dictionary.transform(embedding.reshape(1, -1))  # (1, 2000)
-        sparse_code = sparse_code.flatten()  # (2000, )
-        activated_atoms = sparse_code > 0
-        print("\n\tAtoms: " + str(np.where(sparse_code > 0)))
-        activated_atoms = dictionary.components_[activated_atoms]
-
+        atom_indices, atom_embeddings = get_sparse_code(dictionary,
+                                                        model_wv, word)
         # Get closest words to each atom of discourse
-        for i, atom in enumerate(activated_atoms):
-            word_labels.append(word)
+        for i, atom in zip(atom_indices, atom_embeddings):
+            word_labels.append(f"{word} ({i})")
             demarcation_i += 1
             similar_words = model_wv.similar_by_vector(atom, topn=10)
             print(similar_words)
@@ -99,13 +110,25 @@ if IS_ANALYSIS:
             words, similarities = zip(*similar_words)
             similarities_df[f"{word}_{i}"] = similarities
             words_df[f"{word}_{i}"] = words
+    demarcations.append(demarcation_i)
 
-    ax = sns.heatmap(similarities_df, annot=words_df, fmt="", cmap=cmap,
-                     yticklabels=False)  # Hide y-axis labels
-    ax.vlines(demarcations, *ax.get_ylim())
-    ax.set_xticklabels(word_labels)
-    ax.xaxis.tick_top()
-    ax.tick_params(left=False, top=False)  # Hide ticks
+    idx = [None, None]
+    divisor = demarcations[5]  # Since 11 words in total
+    idx[0] = similarities_df.columns[:divisor]
+    idx[1] = similarities_df.columns[divisor:]
+    word_labels = [word_labels[:divisor], word_labels[divisor:]]
+    demarcations = [demarcations[:6], [x - divisor for x in demarcations[5:]]]
+    fig, axes = plt.subplots(2, 1, sharey=True)
+    for i, ax in enumerate(axes.flat):
+        sns.heatmap(similarities_df[idx[i]], ax=ax,
+                    annot=words_df[idx[i]], fmt="", cmap=cmap,
+                    yticklabels=False,  # Hide y-axis labels
+                    cbar=(i==0))
+        ax.vlines(demarcations[i], *ax.get_ylim(), colors='deepskyblue', lw=4)
+        ax.set_xticklabels(word_labels[i])
+        ax.xaxis.tick_top()
+        ax.tick_params(left=False, top=False)  # Hide ticks
     plt.title(f"{args.model}_{EMBEDDING_SIZE} with {COMPONENT_SIZE} components",
-              y=1.3)
+              y=2.4)
+    plt.savefig(f"results/full_tables/{args.model}_{EMBEDDING_SIZE}_components={COMPONENT_SIZE}.png")
     plt.show()
